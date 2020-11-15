dbb48eaeb818e09dcc7134e3cca3c7c8cc3d9dec
Alikhan219 <merabhai786786@gmail.com>
2020-11-15 09:25:21 -0800

    creating a home componenet and installing the material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes} from "react-router-dom"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route , Link} from "react-router-dom";
 export default function App() {
-  return <Router>
-    <Routes>
-      
-    </Routes>
-  </Router>
+  return (
+    <Router>
+<nav>
+  <Link to="/">Home</Link>
+</nav>
+      <Routes>
+
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to our shoe store!</h1>
+    </div>
+  );
+}
+
 const shoes = {
-  "air-jordan-3-valer-blue":{
+  "air-jordan-3-valer-blue": {
     name: "VALOUR BLUE",
-    img: ""
+    img: "",
   },
-  "jordan-mars-270-londons":{
+  "jordan-mars-270-londons": {
     name: "JORDAN MARS 270 LONDON",
-    img:""
+    img: "",
   },
-  "air-jordan-1-zoom-racer-blue":{
+  "air-jordan-1-zoom-racer-blue": {
     name: "RACER",
-    img: ""
-  }
-}
-
-
+    img: "",
+  },
+};
